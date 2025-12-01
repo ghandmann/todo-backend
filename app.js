be02c4fb3c8f301e35ce74293e8060b9785f5cea
Sven Eppler <git@sveneppler.de>
2025-12-01 10:37:13 +0100

    this is the solution!

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.inMemoryStore = [];
 
 // Gibt Liste aller TodoItem zurück
 app.get("/todo-items/", (req, res) => {
-    res.status(200);
+    res.status(204);
     res.send(app.inMemoryStore);
 });
 
